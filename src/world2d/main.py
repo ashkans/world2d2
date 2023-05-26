247eed93e4e26f6da12c162c7ab47928eadb8d8a
ashkans <61254741+ashkans@users.noreply.github.com>
2023-05-26 17:44:57 +1000

    Create main.py

---
+++ src/world2d/main.py
@@ -0,0 +1,8 @@
+class SquareToCircle(Scene):
+   def construct(self):
+      square = Square()
+      circle = Circle()
+      circle.set_fill(PINK, opacity=0.5)
+      self.play(Create(square))
+      self.play(Transform(square, circle))
+      self.wait()
